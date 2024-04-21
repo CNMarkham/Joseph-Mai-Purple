2b5265e7fa1dc00c851faad5323bb5ef7bf93c8c
Roosham <123781912+MariePlush@users.noreply.github.com>
2024-04-21 15:53:37 -0400

    basically spent like 45 mins on a stupid node problem anyways still like step 102

--- Pac man joseph/Assets/Node.cs
+++ Pac man joseph/Assets/Node.cs
@@ -7,8 +7,28 @@ public class Node : MonoBehaviour
     public LayerMask obstacleLayer;
     public List<Vector2> availableDirections;
 
-    private void Start()
+    void Start()
     {
-        availableDirections = new List<Vector2>();
+         availableDirections = new List<Vector2>();
+
+        CheckAvailableDirection(Vector2.up);
+        CheckAvailableDirection(Vector2.down);
+        CheckAvailableDirection(Vector2.left);
+        CheckAvailableDirection(Vector2.right);
     }
+
+    private void CheckAvailableDirection(Vector2 newDirection)
+    {
+        RaycastHit2D hit = Physics2D.BoxCast(transform.position, Vector2.one * 0.75f, 0f, newDirection, 1f, obstacleLayer);
+
+        if (hit.collider == null)
+        {
+            availableDirections.Add(newDirection);
+
+        }
+        Debug.Log("HIT");
+    }
+
+
+
 }
